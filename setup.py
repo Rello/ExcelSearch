be592115bebe418fdfd7e4f4439ca96537a451e4
Rello <Rello@users.noreply.github.com>
2025-04-26 20:37:59 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+APP = ['search_excel.py']
+DATA_FILES = ['logo.jpg']    # Logo landet später in Contents/Resources
+OPTIONS = {
+    'argv_emulation': True,
+    'packages': ['pandas','openpyxl','PIL'],
+    'iconfile': 'appicon.icns',    # optional, nur wenn Du ein Icon hast
+    'plist': {
+        'CFBundleName': 'ExcelSearcher',
+        'CFBundleIdentifier': 'com.example.ExcelSearcher',
+        'CFBundleVersion': '1.0.0',
+    },
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
